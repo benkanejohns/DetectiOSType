08974a17ef4ad7406387439f65a8ba7d68336270
first_app <benkanejohns@bens-macbook-pro-2.local>
2016-05-11 20:28:34 +1000

    Initial project setup

--- DetectiOSType/GameScene.swift
+++ DetectiOSType/GameScene.swift
@@ -1,26 +1,59 @@
 //
 //  GameScene.swift
-//  DetectiOSType
+//  DeviceTest
 //
-//  Created by Ben Kane Johns on 11/05/2016.
+//  Created by Ben Kane Johns on 9/05/2016.
 //  Copyright (c) 2016 Ben Kane Johns. All rights reserved.
 //
 
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    // Screen size detection
+    let screenSize: CGRect = UIScreen.mainScreen().bounds
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
+        // Access in the current screen width and height
+        let screenWidth = screenSize.width
+        
+        // Request an UITraitCollection instance
+        let deviceIdiom = UIScreen.mainScreen().traitCollection.userInterfaceIdiom
+        
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
+        
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        // Check the idiom to find out the current device type
+        switch (deviceIdiom) {
+            // Display myLabel with the appropriate font size for the device type.
+        case .Phone:
+            switch screenWidth {
+            case 0...320:
+                // iPhone 5 portrait
+                myLabel.fontSize = 41
+            case 321...375:
+                // iPhone 6 portrait
+                myLabel.fontSize = 43
+            default:
+                // iPhone 6 Plus portrait
+                myLabel.fontSize = 45
+            }
+        case .Pad:
+            // iPad 2, Air, Retina and Mini etc Portrait
+            myLabel.fontSize = 58
+        default:
+            break
+        }
         
         self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+        /* Called when a touch begins */
         
         for touch in touches {
             let location = touch.locationInNode(self)
@@ -38,7 +71,7 @@ class GameScene: SKScene {
             self.addChild(sprite)
         }
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
